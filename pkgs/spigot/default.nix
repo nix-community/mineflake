23741c2502b2fb9dd17025e7f469bc1fe26b69f2
Egor Ternovoy <cofob@riseup.net>
2022-08-28 13:28:35 +0300

    Add EssentialsX (#36)
    Closes #34
    
    Reviewed-on: https://git.frsqr.xyz/firesquare/mineflake/pulls/36

--- pkgs/spigot/default.nix
+++ pkgs/spigot/default.nix
@@ -10,4 +10,12 @@ with pkgs; {
   # Plugins
   luckperms = callPackage ./plugins/luckperms { };
   coreprotect = callPackage ./plugins/coreprotect { };
+  essentialsx = callPackage ./plugins/essentialsx { };
+  essentialsx-antibuild = callPackage ./plugins/essentialsx/antibuild.nix { };
+  essentialsx-chat = callPackage ./plugins/essentialsx/chat.nix { };
+  essentialsx-discord = callPackage ./plugins/essentialsx/discord.nix { };
+  essentialsx-geo = callPackage ./plugins/essentialsx/geo.nix { };
+  essentialsx-protect = callPackage ./plugins/essentialsx/protect.nix { };
+  essentialsx-spawn = callPackage ./plugins/essentialsx/spawn.nix { };
+  essentialsx-xmpp = callPackage ./plugins/essentialsx/xmpp.nix { };
 }
---
+++ pkgs/spigot/plugins/essentialsx/protect.nix
@@ -0,0 +1,33 @@
+{ callPackage, lib, stdenv, fetchurl, ... }:
+
+let
+  version = "2.19.8";
+  commit = "0897ef7";
+  buildNum = "1423";
+  pname = "EssentialsXProtect";
+in
+stdenv.mkDerivation {
+  inherit version pname;
+
+  src = fetchurl {
+    url = "https://ci.ender.zone/job/EssentialsX/${buildNum}/artifact/jars/${pname}-${version}-dev+1-${commit}.jar";
+    hash = "sha256-ZZuQGKFhIohaMiMU9jNFafXWc1sqoYhPqsA1SoABuOk=";
+  };
+
+  dontUnpack = true;
+  dontConfigure = true;
+
+  installPhase = "install -Dm444 $src $out";
+
+  meta = with lib; {
+    description = "EssentialsX addon.";
+    homepage = "https://essentialsx.net/";
+    license = licenses.gpl3Only;
+    platforms = platforms.all;
+    deps = [ (callPackage ./default.nix { }) ];
+    configs = { };
+    server = "spigot";
+    type = "result";
+    folders = [ ];
+  };
+}
