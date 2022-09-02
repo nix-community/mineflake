a42f372e5f843b0ef7e46f7b3fb83cbccee901f1
Egor Ternovoy <cofob@riseup.net>
2022-09-02 21:27:11 +0700

    Sort pkgs default

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,19 +1,27 @@
 { pkgs, ... }:
 
 with pkgs; {
+  # MUST BE SORTED! (you can use "Sort Lines" vs code plugin with "Natural" sorting)
+
   # Utils
-  utils = callPackage ./other/utils { };
   lazymc = callPackage ./other/lazymc { };
+  utils = callPackage ./other/utils { };
 
   # Servers
+  bungeecord = callPackage ./servers/bungeecord { };
   paper = callPackage ./servers/paper_1.18.2 { };
   paper_1_18_2 = callPackage ./servers/paper_1.18.2 { };
-  bungeecord = callPackage ./servers/bungeecord { };
   waterfall = callPackage ./servers/waterfall { };
 
   # Plugins
-  luckperms = callPackage ./plugins/luckperms { };
+  advancedban = callPackage ./plugins/advancedban { };
+  authme = callPackage ./plugins/authme { };
+  authmebungee = callPackage ./plugins/authmebungee { };
+  chatty = callPackage ./plugins/chatty { };
+  cleanmotd = callPackage ./plugins/cleanmotd { };
   coreprotect = callPackage ./plugins/coreprotect { };
+  discordsrv = callPackage ./plugins/discordsrv { };
+  elyby = callPackage ./plugins/elyby { };
   essentialsx = callPackage ./plugins/essentialsx { };
   essentialsx-antibuild = callPackage ./plugins/essentialsx/antibuild.nix { };
   essentialsx-chat = callPackage ./plugins/essentialsx/chat.nix { };
@@ -22,25 +30,19 @@ with pkgs; {
   essentialsx-protect = callPackage ./plugins/essentialsx/protect.nix { };
   essentialsx-spawn = callPackage ./plugins/essentialsx/spawn.nix { };
   essentialsx-xmpp = callPackage ./plugins/essentialsx/xmpp.nix { };
-  redlib = callPackage ./plugins/redlib { };
-  vault = callPackage ./plugins/vault { };
-  authme = callPackage ./plugins/authme { };
-  chatty = callPackage ./plugins/chatty { };
-  negativity = callPackage ./plugins/negativity { };
-  tabtps = callPackage ./plugins/tabtps { };
-  protocollib = callPackage ./plugins/protocollib { };
   illegalstack = callPackage ./plugins/illegalstack { };
-  playtime = callPackage ./plugins/playtime { };
-  placeholderapi = callPackage ./plugins/placeholderapi { };
-  lightchatbubbles = callPackage ./plugins/lightchatbubbles { };
-  elyby = callPackage ./plugins/elyby { };
-  authmebungee = callPackage ./plugins/authmebungee { };
-  cleanmotd = callPackage ./plugins/cleanmotd { };
   inventoryrollbackplus = callPackage ./plugins/inventoryrollbackplus { };
+  lightchatbubbles = callPackage ./plugins/lightchatbubbles { };
+  luckperms = callPackage ./plugins/luckperms { };
+  negativity = callPackage ./plugins/negativity { };
+  placeholderapi = callPackage ./plugins/placeholderapi { };
   plasmovoice = callPackage ./plugins/plasmovoice { };
-  discordsrv = callPackage ./plugins/discordsrv { };
+  playtime = callPackage ./plugins/playtime { };
+  protocollib = callPackage ./plugins/protocollib { };
+  redlib = callPackage ./plugins/redlib { };
+  skinsrestorer = callPackage ./plugins/skinsrestorer { };
   tablist = callPackage ./plugins/tablist { };
   tablistbungee = callPackage ./plugins/tablistbungee { };
-  advancedban = callPackage ./plugins/advancedban { };
-  skinsrestorer = callPackage ./plugins/skinsrestorer { };
+  tabtps = callPackage ./plugins/tabtps { };
+  vault = callPackage ./plugins/vault { };
 }
