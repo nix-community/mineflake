b8a47bbca3e5fad6519f57f60e2930196f29b4d2
Egor Ternovoy <cofob@riseup.net>
2022-11-04 03:35:02 +0700

    Fmt

--- cli/default.nix
+++ cli/default.nix
@@ -1,12 +1,14 @@
 { callPackage, fetchFromGitHub, rustPlatform, lib, ... }:
 
 let
-  naersk = callPackage (fetchFromGitHub {
-    owner = "nix-community";
-    repo = "naersk";
-    rev = "6944160c19cb591eb85bbf9b2f2768a935623ed3";
-    sha256 = "sha256-9o2OGQqu4xyLZP9K6kNe1pTHnyPz0Wr3raGYnr9AIgY=";
-  }) { };
+  naersk = callPackage
+    (fetchFromGitHub {
+      owner = "nix-community";
+      repo = "naersk";
+      rev = "6944160c19cb591eb85bbf9b2f2768a935623ed3";
+      sha256 = "sha256-9o2OGQqu4xyLZP9K6kNe1pTHnyPz0Wr3raGYnr9AIgY=";
+    })
+    { };
 in
 naersk.buildPackage rec {
   src = ./.;
