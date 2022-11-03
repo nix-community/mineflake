bca1f5c93a56f1ae851d10b81a660ac8fffb7bf9
Egor Ternovoy <cofob@riseup.net>
2022-11-04 03:29:54 +0700

    Fix mineflake build

--- cli/default.nix
+++ cli/default.nix
@@ -1,15 +1,13 @@
-{ rustPlatform, lib, ... }:
+{ callPackage, fetchFromGitHub, rustPlatform, lib, ... }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "mineflake";
-  version = "0.1.0";
-
+let
+  naersk = callPackage (fetchFromGitHub {
+    owner = "nix-community";
+    repo = "naersk";
+    rev = "6944160c19cb591eb85bbf9b2f2768a935623ed3";
+    sha256 = "sha256-9o2OGQqu4xyLZP9K6kNe1pTHnyPz0Wr3raGYnr9AIgY=";
+  }) { };
+in
+naersk.buildPackage rec {
   src = ./.;
-
-  cargoSha256 = "sha256-wQvUINSxh220xWKzZaSoYeHSS2FhwjZjYIBlO9OoONg=";
-
-  meta = with lib; {
-    description = "CLI that powers Mineflake";
-    license = licenses.mit;
-  };
 }
