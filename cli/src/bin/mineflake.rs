5efe25b77f76a30762a7f2693f1c898aa8e95e69
cofob <cofob@riseup.net>
2022-12-10 20:07:05 +0700

    Update options

--- cli/src/bin/mineflake.rs
+++ cli/src/bin/mineflake.rs
@@ -18,16 +18,19 @@ enum Commands {
     /// Apply a configuration to directory.
     Apply {
         /// Configuration to apply.
+        #[clap(default_value = "mineflake.yml")]
         config: PathBuf,
-        /// Directory to apply configuration.
+        /// Directory to apply configuration. If not specified, the current directory will be used.
         directory: Option<PathBuf>,
     },
 
     /// Generate a configuration.
     Generate {
         /// Server type to generate configuration for. Currently only Bukkit is supported.
+        #[clap(default_value = "bukkit")]
         server: String,
         /// Configuration path. If not specified, a configuration will be printed to stdout.
+        #[clap(long = "output", short = 'o')]
         config: Option<PathBuf>,
     },
 }
