917c67df58c9b9ca57e965324de43e5f5d160907
cofob <cofob@riseup.net>
2022-12-12 12:23:57 +0700

    Reexports

--- cli/src/structures/mod.rs
+++ cli/src/structures/mod.rs
@@ -1,2 +1,4 @@
 pub mod common;
 pub mod spigot;
+
+pub use common::*;
--- cli/src/utils/mod.rs
+++ cli/src/utils/mod.rs
@@ -1,8 +1,10 @@
 pub mod config;
 pub mod linker;
-mod log;
+pub mod log;
 pub mod merge;
 pub mod net;
 
 pub use self::log::initialize_logger;
 pub use config::load_config;
+pub use merge::merge_json;
+pub use net::get_cache_dir;
