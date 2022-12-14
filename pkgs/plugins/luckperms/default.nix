813be2360e1dd0395c9c12e9b357163eac266cd6
cofob <cofob@riseup.net>
2022-12-14 12:55:12 +0700

    Package paper 1.19.3 fully

--- pkgs/plugins/luckperms/default.nix
+++
@@ -1,6 +0,0 @@
-{ lib, stdenv, fetchurl, mineflake, unzip, ... }:
-
-mineflake.buildZipMfPackage {
-  url = mineflake.ipfsUrl "bafybeidnakt6kkphubpi6aye5jet6ftkpt44pstbgtae4a24mojlos7o5i/package.zip";
-  sha256 = "0sxy5q5m4xpv38yk4vmasa5gcq7anv4m1582927s3ym0q1cbqcvv";
-}
