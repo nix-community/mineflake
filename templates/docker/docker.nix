3ea092aa3553de834d35b34310f91e16b565546c
cofob <cofob@riseup.net>
2022-12-14 13:02:36 +0700

    Update example

--- templates/docker/docker.nix
+++ templates/docker/docker.nix
@@ -3,8 +3,10 @@
 with pkgs; mineflake.buildMineflakeContainer {
   package = mineflake.paper;
   command = "${jdk}/bin/java -Xms1G -Xmx1G -jar {} nogui";
-  plugins = with mineflake; [ authme ];
+  plugins = with mineflake; [ luckperms ];
   configs = [
-    (mineflake.mkMfConfig "raw" "eula.txt" "eula=true")
+    (mineflake.mkMfConfig "mergeyaml" "plugins/LuckPerms/config.yml" {
+      server = "vanilla_1";
+    })
   ];
 }
