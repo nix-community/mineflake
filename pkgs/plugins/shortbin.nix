8b5d7c44fd16415ad806f7ae0f3ee633df16d09b
cofob <cofob@riseup.net>
2022-12-15 09:13:17 +0700

    init: AuthMeReloaded 5.6.0-beta2

--- pkgs/plugins/shortbin.nix
+++ pkgs/plugins/shortbin.nix
@@ -5,4 +5,9 @@ rec {
     url = mineflake.ipfsUrl "bafybeidnakt6kkphubpi6aye5jet6ftkpt44pstbgtae4a24mojlos7o5i/package.zip";
     sha256 = "0sxy5q5m4xpv38yk4vmasa5gcq7anv4m1582927s3ym0q1cbqcvv";
   };
+
+  authme = mineflake.buildZipMfPackage {
+    url = mineflake.ipfsUrl "bafybeifox6mekis77muueh4nzdjbf3kjvxdp7vztu4hr47lqnui5p3sr6i/authme.zip";
+    sha256 = "1l0ax3v4mk41g2zw9p17mff5lawbhr76nak2lgmw2c4qc16iisgl";
+  };
 }
