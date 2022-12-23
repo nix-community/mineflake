031228f6c91c15ad01e260343d44d98133d5d899
AveryanAlex <alex@averyan.ru>
2022-12-23 13:15:15 +0300

    Per-version waterfall

--- pkgs/servers/default.nix
+++ pkgs/servers/default.nix
@@ -1,5 +1,6 @@
 { callPackage, ... }:
 
-{
-  waterfall = callPackage ./waterfall { };
+rec {
+  waterfall_1-19-3 = callPackage ./waterfall { };
+  waterfall = waterfall_1-19-3;
 } // (callPackage ./shortbin.nix { })
