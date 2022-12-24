0ea4c79ed05086b4c0625b5116437d7777dbb5fb
Alexander Averyanov <alex@averyan.ru>
2022-12-24 12:28:31 +0300

    1-19-3 -> 1-19
    Co-authored-by: Egor Tenovoy <cofob@riseup.net>

--- pkgs/servers/default.nix
+++ pkgs/servers/default.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }:
 
 rec {
-  waterfall_1-19-3 = callPackage ./waterfall { };
-  waterfall = waterfall_1-19-3;
+  waterfall_1-19 = callPackage ./waterfall { };
+  waterfall = waterfall_1-19;
 } // (callPackage ./shortbin.nix { })
